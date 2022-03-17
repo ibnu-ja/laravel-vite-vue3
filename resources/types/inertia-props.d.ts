a13f3666f35d118923676e78caba8eb61ea50087
Ibnu <ibnu.magang@gmail.com>
2022-03-17 14:40:59 +0700

    chore: remove semicolon

--- resources/types/inertia-props.d.ts
+++ resources/types/inertia-props.d.ts
@@ -1,29 +1,29 @@
 /* eslint-disable camelcase */
 export type User = {
-  id: number;
-  name: string;
-  email: string;
-  email_verified_at?: Date;
-  current_team_id?: number;
-  profile_photo_path?: string;
-  created_at: Date;
-  updated_at: Date;
-  profile_photo_url?: string;
-  owned_teams?: any[];
-  current_team?: null;
-  all_teams?: any[];
-  two_factor_enabled?: boolean;
+  id: number
+  name: string
+  email: string
+  email_verified_at?: Date
+  current_team_id?: number
+  profile_photo_path?: string
+  created_at: Date
+  updated_at: Date
+  profile_photo_url?: string
+  owned_teams?: any[]
+  current_team?: null
+  all_teams?: any[]
+  two_factor_enabled?: boolean
 }
 
 export type Jetstream = {
-  canCreateTeams?: boolean;
-  canManageTwoFactorAuthentication?: boolean;
-  canUpdatePassword?: boolean;
-  canUpdateProfileInformation?: boolean;
-  flash?: any;
-  hasAccountDeletionFeatures?: boolean;
-  hasApiFeatures?: boolean;
-  hasTeamFeatures?: boolean;
-  hasTermsAndPrivacyPolicyFeature?: boolean;
-  managesProfilePhotos?: boolean;
+  canCreateTeams?: boolean
+  canManageTwoFactorAuthentication?: boolean
+  canUpdatePassword?: boolean
+  canUpdateProfileInformation?: boolean
+  flash?: any
+  hasAccountDeletionFeatures?: boolean
+  hasApiFeatures?: boolean
+  hasTeamFeatures?: boolean
+  hasTermsAndPrivacyPolicyFeature?: boolean
+  managesProfilePhotos?: boolean
 }
