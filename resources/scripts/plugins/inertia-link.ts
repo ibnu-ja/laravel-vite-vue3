50cb748788374d227a756490d4f9d3e5a3391cb0
Ibnu <ibnu.magang@gmail.com>
2022-03-17 11:57:00 +0700

    chore(Frontend/Plugins): :fire: Remove inertia-link plugins

--- resources/scripts/plugins/inertia-link.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable sort-imports */
-import { h } from 'vue'
-import { Link } from '@inertiajs/inertia-vue3'
-
-const InertiaLink = (props, { slots, attrs }) => {
-  return h(Link, { ...attrs, style: { color: 'inherit', 'text-decoration': 'inherit' } }, slots)
-}
-
-export default InertiaLink
