505a483780bf3600f1cdebdebe7c3206e009203e
Ibnu <ibnu.magang@gmail.com>
2022-03-17 19:56:41 +0700

    fix(userPage): fix lauout & margin

--- resources/views/pages/Profile/Show.vue
+++ resources/views/pages/Profile/Show.vue
@@ -1,62 +1,59 @@
 <script lang="ts" setup>
 import { Jetstream, User } from '@/types/inertia-props'
 import AppLayout from '@/views/layouts/AppLayout.vue'
-import DeleteUserForm from '@/views/pages/Profile/Partials/DeleteUserForm.vue'
-import JetSectionBorder from '@/views/Jetstream/SectionBorder.vue'
-import LogoutOtherBrowserSessionsForm from '@/views/pages/Profile/Partials/LogoutOtherBrowserSessionsForm.vue'
-import TwoFactorAuthenticationForm from '@/views/pages/Profile/Partials/TwoFactorAuthenticationForm.vue'
-import UpdatePasswordForm from '@/views/pages/Profile/Partials/UpdatePasswordForm.vue'
-import UpdateProfileInformationForm from '@/views/pages/Profile/Partials/UpdateProfileInformationForm.vue'
+import DeleteUserForm from './Partials/DeleteUserForm.vue'
+import LogoutOtherBrowserSessionsForm from './Partials/LogoutOtherBrowserSessionsForm.vue'
+import { Session } from '@/types/session'
+import TwoFactorAuthenticationForm from './Partials/TwoFactorAuthenticationForm.vue'
+import UpdatePasswordForm from './Partials/UpdatePasswordForm.vue'
+import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm.vue'
 import { computed } from 'vue'
 import { usePage } from '@inertiajs/inertia-vue3'
 
-const jetstream = computed(() => usePage().props.value.jetstream as Jetstream)
-// const page = usePage()
+defineProps<{sessions: Session[]}>()
 
 const user = computed(() => usePage().props.value.user as User)
-// const user = ref(computed(() => usePage().props.value.user))
-defineProps<{
-  sessions: any[]
-}>()
+const jetstream = computed(() => usePage().props.value.jetstream as Jetstream)
+
 </script>
 
 <template>
-  <app-layout title="Profile">
+  <app-layout>
     <template #header>
-      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+      <h2>
         Profile
       </h2>
     </template>
 
     <div>
-      <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+      <div class="mx-auto py-10 px-sm-6 px-lg-8">
         <div v-if="jetstream.canUpdateProfileInformation">
           <update-profile-information-form :user="user" />
 
-          <jet-section-border />
+          <v-divider class="my-8" />
         </div>
 
         <div v-if="jetstream.canUpdatePassword">
-          <update-password-form class="mt-10 sm:mt-0" />
+          <update-password-form class="mt-10 mt-sm-0" />
 
-          <jet-section-border />
+          <v-divider class="my-8" />
         </div>
 
         <div v-if="jetstream.canManageTwoFactorAuthentication">
-          <two-factor-authentication-form class="mt-10 sm:mt-0" />
+          <two-factor-authentication-form class="mt-10 mt-sm-0" />
 
-          <jet-section-border />
+          <v-divider class="my-8" />
         </div>
 
         <logout-other-browser-sessions-form
           :sessions="sessions"
-          class="mt-10 sm:mt-0"
+          class="mt-10 mt-sm-0"
         />
 
         <template v-if="jetstream.hasAccountDeletionFeatures">
-          <jet-section-border />
+          <v-divider class="my-8" />
 
-          <delete-user-form />
+          <delete-user-form class="mt-10 mt-sm-0" />
         </template>
       </div>
     </div>
