b0ce00f57c0acc851ed7ccbf6ca66055679fd94c
Ibnu <ibnu.magang@gmail.com>
2022-03-17 13:26:30 +0700

    feat(Frontend/Pages/User): DeleteUserForm

---
+++ resources/views/pages/Profile/Partials/DeleteUserForm.vue
@@ -0,0 +1,109 @@
+<template>
+  <app-action-section>
+    <template #title>
+      Delete Account
+    </template>
+
+    <template #description>
+      Permanently delete your account.
+    </template>
+
+    <template #content>
+      <div class="text-sm">
+        Once your account is deleted, all of its resources and data will be
+        permanently deleted. Before deleting your account, please download any
+        data or information that you wish to retain.
+      </div>
+    </template>
+    <template #actions>
+      <v-dialog v-model="confirmingUserDeletion">
+        <v-form @submit.prevent="deleteUser">
+          <v-card>
+            <v-card-title class="headline">
+              Delete Account
+            </v-card-title>
+
+            <v-card-text>
+              Are you sure you want to delete your account? Once your account is
+              deleted, all of its resources and data will be permanently deleted.
+              Please enter your password to confirm you would like to permanently
+              delete your account.
+
+              <div class="mt-4">
+                <v-text-field
+                  v-model="form.password"
+                  type="password"
+                  class="mt-1"
+                  label="Password"
+                  ref="password"
+                  variant="outlined"
+                  :error-messages="Object.values(form.errors)"
+                />
+              </div>
+            </v-card-text>
+
+            <v-card-actions class="px-6 py-4">
+              <v-spacer />
+              <v-btn
+                outlined
+                @click="closeModal"
+              >
+                Nevermind
+              </v-btn>
+
+              <v-btn
+                color="error"
+                :class="{ 'opacity-25': form.processing }"
+                :disabled="form.processing"
+                type="submit"
+              >
+                Delete Account
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-form>
+      </v-dialog>
+      <v-btn
+        color="error"
+        @click="confirmUserDeletion"
+      >
+        Delete Account
+      </v-btn>
+    </template>
+  </app-action-section>
+</template>
+
+<script lang="ts" setup>
+import { inject, ref } from 'vue'
+import AppActionSection from '@/views/components/ActionSection.vue'
+import { useForm } from '@inertiajs/inertia-vue3'
+
+const route : any = inject('route')
+const confirmingUserDeletion = ref(false)
+const form = ref(useForm({
+  password: ''
+}))
+const password = ref<HTMLInputElement>()
+
+function confirmUserDeletion () {
+  confirmingUserDeletion.value = true
+  // TODO call focus() and reset() when there is support
+  // setTimeout(() => password.value?.focus(), 300)
+}
+
+function deleteUser () {
+  form.value.delete(route('current-user.destroy'), {
+    preserveScroll: true,
+    onSuccess: () => closeModal()
+    // onError: () => password.value?.focus(),
+    // onFinish: () => form.value.reset()
+  })
+}
+
+function closeModal () {
+  confirmingUserDeletion.value = false
+  // form.value.reset()
+}
+
+// defineExpose(password)
+</script>
