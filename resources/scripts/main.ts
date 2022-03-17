583c2182a92e3627c6dbf77dd8934b73880e3561
Ibnu <ibnu.magang@gmail.com>
2022-03-17 23:22:30 +0700

    feat(NPM): add @inertiajs/progress

--- resources/scripts/main.ts
+++ resources/scripts/main.ts
@@ -6,10 +6,9 @@ import ZiggyVue from './plugins/ziggy-vue'
 // eslint-disable-next-line sort-imports
 import { Link, createInertiaApp } from '@inertiajs/inertia-vue3'
 import { importPageComponent } from '@/scripts/vite/import-page-component'
-// import RouterLink from '@/scripts/plugins/RouterLink'
+import { InertiaProgress } from '@inertiajs/progress'
 import pinia from '@/scripts/plugins/pinia'
 import vuetify from '@/scripts/plugins/vuetify'
-// import InertiaLink from './plugins/inertia-link'
 createInertiaApp({
   resolve: (name) => importPageComponent(name, import.meta.glob('../views/pages/**/*.vue')),
   setup ({ el, app, props, plugin }) {
@@ -18,7 +17,8 @@ createInertiaApp({
       .use(pinia)
       .use(ZiggyVue, Ziggy)
       .use(vuetify)
-      // .component('InertiaLink', InertiaLink)
       .mount(el)
   }
 })
+
+InertiaProgress.init()
