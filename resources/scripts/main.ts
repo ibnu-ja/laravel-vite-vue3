580e8865633fc3832dbd947db47c020bccbf2a9c
Ibnu <ibnu.magang@gmail.com>
2022-03-17 23:23:20 +0700

    chore: main.ts import order

--- resources/scripts/main.ts
+++ resources/scripts/main.ts
@@ -1,12 +1,9 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable sort-imports */
 import { createApp, h } from 'vue'
+import { InertiaProgress } from '@inertiajs/progress'
 import { Ziggy } from './ziggy'
 import ZiggyVue from './plugins/ziggy-vue'
-// eslint-disable-next-line sort-imports
-import { Link, createInertiaApp } from '@inertiajs/inertia-vue3'
+import { createInertiaApp } from '@inertiajs/inertia-vue3'
 import { importPageComponent } from '@/scripts/vite/import-page-component'
-import { InertiaProgress } from '@inertiajs/progress'
 import pinia from '@/scripts/plugins/pinia'
 import vuetify from '@/scripts/plugins/vuetify'
 createInertiaApp({
