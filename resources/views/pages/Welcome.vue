bfbbff16bb176c07c43e4c3b13fc5b7b247bfa9f
Ibnu <ibnu.magang@gmail.com>
2022-03-19 04:20:02 +0700

    feat(welcome): use HelloWorld component

---
+++ resources/views/pages/Welcome.vue
@@ -0,0 +1,11 @@
+
+<script lang="ts" setup>
+import HelloWorld from '@/views/components/HelloWorld.vue'
+import WebLayout from '@/views/layouts/WebLayout.vue'
+</script>
+
+<template>
+  <web-layout>
+    <hello-world />
+  </web-layout>
+</template>
