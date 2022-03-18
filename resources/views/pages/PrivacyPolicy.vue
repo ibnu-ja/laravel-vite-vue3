bbe1d2fb38b7911235cdabe82552c9bec645ff71
Ibnu <ibnu.magang@gmail.com>
2022-03-19 04:17:05 +0700

    feat: update terms and privacy policy page

---
+++ resources/views/pages/PrivacyPolicy.vue
@@ -0,0 +1,10 @@
+
+<script lang="ts" setup>
+import WebLayout from '@/views/layouts/WebLayout.vue'
+defineProps<{policy: 'string'}>()
+</script>
+<template>
+  <web-layout>
+    <div v-html="policy" />
+  </web-layout>
+</template>
---
+++ resources/views/pages/TermsOfService.vue
@@ -0,0 +1,10 @@
+
+<script lang="ts" setup>
+import WebLayout from '@/views/layouts/WebLayout.vue'
+defineProps<{terms: 'string'}>()
+</script>
+<template>
+  <web-layout>
+    <div v-html="terms" />
+  </web-layout>
+</template>
